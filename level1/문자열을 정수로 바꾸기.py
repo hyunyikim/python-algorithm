55477554dc3dcce903ebc06ec34331e26226e7ff
hyunyikim <developerhaileykim@gmail.com>
2019-04-01 23:44:02 +0900

    문자열을 정수로 바꾸기.py

---
+++ level1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,13 @@
+# 출처 프로그래머스 : https://programmers.co.kr/learn/courses/30/lessons/12925?language=python3
+
+
+def solution(s):
+    answer = 0
+    answer = int(s)
+
+    print(type(answer), answer)
+    return answer
+
+
+solution('1234')
+solution('-1234')
